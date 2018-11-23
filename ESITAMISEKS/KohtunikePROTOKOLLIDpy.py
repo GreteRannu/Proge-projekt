b9415af1f046839b3c631dac94714eb8f368f883
Grete Rannu <grete.rannu@gmail.com>
2018-11-23 12:29:35 +0200

    Nupu funktsioon muudetud

--- ESITAMISEKS/KohtunikePROTOKOLLIDpy.py
+++ ESITAMISEKS/KohtunikePROTOKOLLIDpy.py
@@ -1,37 +1,4 @@
-"""
-Programm: Lohesurfi vastiili hindavate kohtunike hindamislehed ja protokoll
-Autor: Kerli Otti
 
-Kirjutasin antud programmi reaalsest vajadusest.
-Programm on edasiarendus Programmeerimine I lõputööst.
-Lohesurfi võistlust hinnates peab kohtunik jälgima suure tuule ja vihma käes
-võistlejat ning suutma kiirelt kirja panema nähtud soorituse (triki) ja selle hinde.
-Iga kohtuniku hindamisest tuleb säilitada protokoll.
-
-Programm:
-a)  Registreerib hindamislehti täitva kohtuniku nime ja aja, mil ta võistlejat hindab.
-b)  Jälgib reeglites lubatud soorituste arvu ühe võistleja kohta (max 7).
-c)  Avab nuppudega hindamislehe, kus iga triki (nupu nimi) on võetud failist.
-    Iga nupp on seotud failist saadud väärtusega (rida-kahekordne järjend-sõnastik).
-    Kui nuppu vajutatakse, siis lisatakse hindeks vastava triki väärtuse e. punktisumma.
-d)  Võtab osalejate nimed failist ja laseb hinnata a) lubatud soorituste arvuni  või b) kui
-    vajutatakse nuppu "VÄLJU LEHELT". Eraldi nupuna on "LISA -1", millega saab võtta punkte maha,
-    kui sooritus ei olnud väärtusele kohane. Miinused arvutatakse hiljem maha punktisummast.
-e)  Kirjutab kohtuniku hindamisest protokolli iga võistleja kohta (automaatne aeg, nimi, soorituste
-    hinded, miinused, punktisumma kokku ja soorituste keskmine hinne) 
-f)  Kirjutab võistleja lõpliku punktisummaeraldi faili. Sellesse faili peaksid salvestuma ka
-    teiste kohtunike poolt saadud tulemused ja mida saab tulevikus kasutada lõplike võistlustulemuste
-    genereerimisel. 
-
-Vajalikud failid:
-Nuppudele nime ja väärtuse jaoks kuni 12 reaga fail, milles sisaldub ühel real triki nimi ja arvuline
-väärtus (float) eraldatud tühikuga (trikid.txt)
-Osalejate registreerimisleht e võistlejate nimedega fail (võistlejad.txt)
-Antud hinnete kokkuvõtted e protokoll (protokoll.txt)
-Tulemuste fail, kuhu salvestatakse vaid võistleja nimi ja punktisumma (tulemused.txt)
-Graafiline programm (easygui.py)
-
-"""
 from easygui import *
 from tkinter import *
 from tkinter import ttk # platvormi ühise stiili saamiseks
@@ -81,13 +48,14 @@ c=[]                    #list ainult trikkide nimedest,
 for element in b:       #mis lisatakse nuppude text= ..... väärtusteks
     c.append(element)
 
+    
 #....................................................................................
 """12 triki nime ja väärtusega hindamispaneel
 NUPUD EKRAANILE, mida kasutatakse võistluse ajal kohtuniku poolt"""
 
 def avame_hinnetelehe(võistleja):
     win = Tk()
-    win.geometry("1080x1900") #siin saab muuta hindamisel kasutatava ekraani mõõtmeid
+    win.geometry("{0}x{1}+0+0".format(win.winfo_screenwidth(), win.winfo_screenheight())) #siin saab muuta hindamisel kasutatava ekraani mõõtmeid
     win.title("HINDAMISLEHT")
     a=[] #Siia kogutakse võistlejale antud hinded selle kohtuniku pool
     h=2 #saab muuta nummude kaugust omavahel
@@ -95,6 +63,7 @@ def avame_hinnetelehe(võistleja):
     r=20
     katseid=[]
     miinus=[]
+    
     b1 = Button(win,text=c[i], height=h, width= r)
     b2 = Button(win,text=c[i+1], height=h, width= r)
     b3 = Button(win,text=c[i+2], height=h, width= r)
@@ -107,6 +76,19 @@ def avame_hinnetelehe(võistleja):
     b10 = Button(win,text=c[i+9], height=h, width= r)
     b11 = Button(win,text=c[i+10], height=h, width= r)
     b12 = Button(win,text=c[i+11], height=h, width= r)
+    
+    b13 = Button(win,text=c[i], height=h, width= r)
+    b14 = Button(win,text=c[i+1], height=h, width= r)
+    b15 = Button(win,text=c[i+2], height=h, width= r)
+    b16 = Button(win,text=c[i+3], height=h, width= r)
+    b17 = Button(win,text=c[i+4], height=h, width= r)
+    b18 = Button(win,text=c[i+5], height=h, width= r)
+    b19 = Button(win,text=c[i+6], height=h, width= r)
+    b20 = Button(win,text=c[i+7], height=h, width= r)
+    b21 = Button(win,text=c[i+8], height=h, width= r)
+    b22 = Button(win,text=c[i+9], height=h, width= r)
+    b23 = Button(win,text=c[i+10], height=h, width= r)
+    b24 = Button(win,text=c[i+11], height=h, width= r)
 
     b1.grid(row=5, column=1)
     b2.grid(row=6, column=1)
@@ -120,6 +102,19 @@ def avame_hinnetelehe(võistleja):
     b10.grid(row=6, column=3)
     b11.grid(row=7, column=3)
     b12.grid(row=8, column=3)
+    
+    b13.grid(row=5, column=5)
+    b14.grid(row=6, column=5)
+    b15.grid(row=7, column=5)
+    b16.grid(row=8, column=5)
+    b17.grid(row=5, column=6)
+    b18.grid(row=6, column=6)
+    b19.grid(row=7, column=6)
+    b20.grid(row=8, column=6)
+    b21.grid(row=5, column=7)
+    b22.grid(row=6, column=7)
+    b23.grid(row=7, column=7)
+    b24.grid(row=8, column=7)
 
     #................................................
     Label(win, text="VÕISTLEJA:").grid(row=1)
@@ -127,56 +122,35 @@ def avame_hinnetelehe(võistleja):
     Label(win, text="VÕISTLEJA:").grid(row=1)
     Label(win, text="HINDAV KOHTUNIK:").grid(row=1, column=4)
     Label(win, text=kohtunik).grid(row=2, column=4)
+    Label(win, text="Antud punktid:").grid(row = 4, column = 4)
+    Label(win, text="Hetkel pukte:").grid(row = 6, column = 4)
     välju= Button(win, text='KATKESTAS')
     välju.grid(row=3)
     min = Button(win, text=' LISA -1 ')
     min.grid(row=4)
     #trikke_mitu=Button(win, text=' TRIKKE "0" ')
     #trikke_mitu.grid(row=5)
-    
+    def naita_punkte(a):
+        for el in a:
+            "\n".join
+        Label(win, text = a). grid(row = 5, column = 4)
+    def hetkel_punkte(a):
+        Label(win, text = sum(a)) .grid(row = 7, column = 4)
+        
     #................................................
     """12 nupu vajutamisega kaasnevad funktsioonid"""
     #selleks, et nupule vajutamisel tekiks mitmekordne järjend saadud hinnetest,
     #mida hiljem töödelda/protokollida. str tehakse floatiks, et
     # srvutada hinnete keskmiseid, summat saadud järjendist
-    def nupp1():
-        a.append(float(b[c[i]]))
+    def nupp(x):
+        a.append(float(b[c[x]]))
+        #b1 = Button(win,text=c[i], height=h, width= r)
         lisa_katse()
-    def nupp2():
-        a.append(float(b[c[i+1]]))
-        lisa_katse()
-    def nupp3():
-        a.append(float(b[c[i+2]]))
-        lisa_katse()
-    def nupp4():
-        a.append(float(b[c[i+3]]))
-        lisa_katse()
-    def nupp5():
-        a.append(float(b[c[i+4]]))
-        lisa_katse()
-    def nupp6():
-        a.append(float(b[c[i+5]]))
-        lisa_katse()
-    def nupp7():
-        a.append(float(b[c[i+6]]))
-        lisa_katse()
-    def nupp8():
-        a.append(float(b[c[i+7]]))
-        lisa_katse()
-    def nupp9():
-        a.append(float(b[c[i+8]]))
-        lisa_katse()
-    def nupp10():
-        a.append(float(b[c[i+9]]))
-        lisa_katse()
-    def nupp11():
-        a.append(float(b[c[i+10]]))
-        lisa_katse()
-    def nupp12():
-        a.append(float(b[c[i+11]]))
-        lisa_katse()
+        naita_punkte(a)
+        hetkel_punkte(a)
+    
     def välju1(): # rakendub kui katsete arv on täis
-        if a == []: # kui pole trikke, siis rpannakse kõik 0
+        if a == []: # kui pole trikke, siis pannakse kõik 0
             trikke()
         hinded.append(a)#lisab saadud hinded hined[] listi
         miinused.append(sum(miinus))
@@ -195,30 +169,49 @@ def avame_hinnetelehe(võistleja):
                   
         
     #.......................................................................
-    b1.configure(command=nupp1)         #Siin on seadistatud, millis juhtub
-    b2.configure(command=nupp2)         # nupu vajutamisel ja milline
-    b3.configure(command=nupp3)         # väärtus b=[] järjendist lisatakse
-    b4.configure(command=nupp4)         # a=[] listi e. saadud hinnete listi
-    b5.configure(command=nupp5)
-    b6.configure(command=nupp6)
-    b7.configure(command=nupp7)
-    b8.configure(command=nupp8)
-    b9.configure(command=nupp9)
-    b10.configure(command=nupp10)
-    b11.configure(command=nupp11)
-    b12.configure(command=nupp12)
+    b1.configure(command=lambda :nupp(1))         #Siin on seadistatud, millis juhtub
+    b2.configure(command=lambda :nupp(2))         # nupu vajutamisel ja milline
+    b3.configure(command=lambda :nupp(3))         # väärtus b=[] järjendist lisatakse
+    b4.configure(command=lambda :nupp(4))         # a=[] listi e. saadud hinnete listi
+    b5.configure(command=lambda :nupp(5))
+    b6.configure(command= lambda :nupp(6))
+    b7.configure(command=lambda :nupp(7))
+    b8.configure(command=lambda :nupp(8))
+    b9.configure(command=lambda :nupp(9))
+    b10.configure(command=lambda :nupp(10))
+    b11.configure(command=lambda :nupp(11))
+    b12.configure(command=lambda :nupp(12))
+    b13.configure(command=lambda :nupp(1))         #Siin on seadistatud, millis juhtub
+    b14.configure(command=lambda :nupp(2))         # nupu vajutamisel ja milline
+    b15.configure(command=lambda :nupp(3))         # väärtus b=[] järjendist lisatakse
+    b16.configure(command=lambda :nupp(4))         # a=[] listi e. saadud hinnete listi
+    b17.configure(command=lambda :nupp(5))
+    b18.configure(command=lambda :nupp(6))
+    b19.configure(command=lambda :nupp(7))
+    b20.configure(command=lambda :nupp(8))
+    b21.configure(command=lambda :nupp(9))
+    b22.configure(command=lambda :nupp(10))
+    b23.configure(command=lambda :nupp(11))
+    b24.configure(command=lambda :nupp(12))
+
     välju.configure(command=välju1)
     min.configure(command=miinus1)
+    
     #trikke_polnud.configure(command=trikke)
     mainloop( )
+    
+
+
 
 i=0 
 nr = 1 #Hindamislehed on nummerdatud
 hinded=[]
 miinused=[]
+
 for read in f:
     võistleja = read.upper() #loeb registreerinud võistlejate failist esimese võistleja nime. Peale tsükli läbimist järgmise võistleja.
-    avame_hinnetelehe(võistleja) #lisab failist saadud võistleja nime hinnetelehele
+    avame_hinnetelehe(võistleja) #avame_hinnetelehe(võistleja) #lisab failist saadud võistleja nime hinnetelehele
+   
     summa = sum(hinded[i])
     hinne = summa - miinused[i]
     keskmine = round((hinne)/ len(hinded[i]), 1)
